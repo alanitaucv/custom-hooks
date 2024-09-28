78a85a9309a4725219d16c93d09e6541b7eade36
Zilerimar Fernandez <alanitaucv@gmail.com>
2024-09-27 22:57:06 -0300

    primer commit

---
+++ useTodo.js
@@ -0,0 +1,50 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-reducer/todoReducer";
+
+export const useTodo = () => {
+
+    const initialState = [];
+
+    const init = () => {
+        return JSON.parse(localStorage.getItem('todos')) || []
+    }
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+    
+    const handlerNewTodo = ( todo ) => {
+        const action = {
+            type: 'Add Todo',
+            payload: todo
+        }
+        dispatch (action);
+    }
+
+    const handlerDeleteTodo = ( id ) => {
+        const action = {
+            type: 'Remove Todo',
+            payload: id
+        }
+        dispatch(action);
+    }
+
+    const handlerToggleTodo = ( id ) => {
+        const action = {
+            type: 'Toggle Todo',
+            payload: id
+        }
+        dispatch(action);
+    }
+
+    return {
+        todos,
+        handlerNewTodo,
+        handlerDeleteTodo,
+        handlerToggleTodo,
+        todosCount: todos.length, 
+        pendingTodosCount: todos.filter( todo => !todo.done).length
+    }
+}
